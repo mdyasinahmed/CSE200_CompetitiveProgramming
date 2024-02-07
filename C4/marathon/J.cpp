634b660145645f7aca087796a2f9f180ffcf7e33
mdyasinahmed <yasinahmed7875@gmail.com>
2024-02-07 17:13:54 +0600

    update solved code J

--- C4/marathon/J.cpp
+++ C4/marathon/J.cpp
@@ -1,23 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int MOD = 1000000007;
+
+int summingSeries(int s){
+    int sum = 0;
+    for(int i=1; i<=s; i++) {
+        int n = pow(i, 2) - pow(i-1, 2);
+        sum+=n;
+        n=0;
+    }
+
+    return (sum)%MOD;
+}
+
 int main() {
     int t;
     cin >> t;
 
     while(t--) {
-
         int n;
         cin >> n;
-
-        int sum = 0;
-        for(int i=1; i<=n; i++) {
-            int T = pow(i, 2) - pow(i-1, 2);
-            sum+=T;
-            T=0;
-        }
-
-        cout << sum << endl;
+        cout << summingSeries(n) << endl;
     }
 
     return 0;
