b546d38f58b0cb9a6a0f2bb13bc6b30c5065fad4
mdyasinahmed <yasinahmed7875@gmail.com>
2024-02-07 10:40:38 +0600

    add solved problem code

---
+++ C4/marathon/F.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, count=0;
+    cin >> t;
+
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+
+        if(b-a >= 2) {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
